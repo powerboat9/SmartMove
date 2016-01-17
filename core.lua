9c39ec0452e7b43caf050365c9621e68facba3b8
powerboat9 <powerboat9@users.noreply.github.com>
2016-01-17 08:39:36 -0500

    Did some work.

--- core.lua
+++ core.lua
@@ -8,8 +8,23 @@ local back = turtle.back
 local xPos, yPos, zPos = 0, 0, 0
 
 if fs.exists("SmartMove/data") and (not isDir("SmartMove/data") then
-    local file = fs.open("SmartMove/data", "w")
+    local file = fs.open("SmartMove/data", "r")
+    local data = textutils.unserialize(file.readAll())
+    xPos, yPos, zPos = data.x, data.y, data.z
+    file.close()
+end
 
 function saveChanges()
-    local x, y, z = coroutine.yield()
-    local file = fs.open("SmartMove/data", "
+    while true do
+        local x, y, z = coroutine.yield()
+        xPos, yPos, zPos = xPos + x, yPos + y, zPos + z
+        local file = fs.open("SmartMove/data", "w")
+        local writeData = {
+            ["x"] = xPos,
+            ["y"] = yPos,
+            ["z"] = zPos
+        }
+        file.write(textutils.serialize(writeData))
+        file.close()
+    end
+end
