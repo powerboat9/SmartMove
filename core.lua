2df60e4468a2b9cda0ea57b8e494a4e521c6850e
powerboat9 <powerboat9@users.noreply.github.com>
2016-01-17 10:48:36 -0500

    Did work.

--- core.lua
+++ core.lua
@@ -5,26 +5,87 @@ local right = turtle.turnRight
 local forward = turtle.forward
 local back = turtle.back
 
-local xPos, yPos, zPos = 0, 0, 0
+local xPos, yPos, zPos, rot = false, false, false, false
+
+function wipe()
+    term.clear()
+    term.setCursorPos(1, 1)
+end
 
 if fs.exists("SmartMove/data") and (not isDir("SmartMove/data") then
     local file = fs.open("SmartMove/data", "r")
     local data = textutils.unserialize(file.readAll())
-    xPos, yPos, zPos = data.x, data.y, data.z
+    xPos, yPos, zPos, rot = data.x, data.y, data.z, data.r
     file.close()
+else
+    while true do
+        wipe()
+        term.write("Enter X: ")
+        xPos = tonumber(read())
+        if xPos then
+            break
+        end
+        term.write("Invalid X")
+        sleep(1)
+    end
+    while true do
+        wipe()
+        term.write("Enter Y: ")
+        yPos = tonumber(read())
+        if yPos then
+            break
+        end
+        term.write("Invalid Y")
+        sleep(1)
+    end
+    while true do
+        wipe()
+        term.write("Enter Z: ")
+        zPos = tonumber(read())
+        if zPos then
+            break
+        end
+        term.write("Invalid Z")
+        sleep(1)
+    end
+    while true do
+        wipe()
+        term.write("Enter Rotation: ")
+        local getRotGUI = read()
+        if type(getRotGUI) == "string" then
+            rot = ({
+                ["north"] = 1,
+                ["east"] = 2,
+                ["south"] = 3,
+                ["west"] = 4
+            })[string.lower(getRotGUI)]
+        elseif type(getRotGUI) == number then
+            rot = getRotGUI
+        end
+        if rot and (rot >= 1) and (rot <= 4) then
+            break
+        end
+        term.write("Invalid Rotation")
+        sleep(1)
+    end
 end
 
-function saveChanges()
+local saveChanges = coroutine.create(function()
     while true do
-        local x, y, z = coroutine.yield()
-        xPos, yPos, zPos = xPos + x, yPos + y, zPos + z
+        local x, y, z, r = coroutine.yield()
+        xPos, yPos, zPos, r = xPos + x, yPos + y, zPos + z, rot + r
         local file = fs.open("SmartMove/data", "w")
         local writeData = {
             ["x"] = xPos,
             ["y"] = yPos,
-            ["z"] = zPos
+            ["z"] = zPos,
+            ["r"] = rot
         }
         file.write(textutils.serialize(writeData))
         file.close()
     end
-end
+end)
+
+local moveTurtle = coroutine.create(function()
+    while true do
+        
